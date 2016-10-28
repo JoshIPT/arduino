0ee50f33ef874f0ee1a8491c1ec0511db9f8850e
JoshTPT <josh@tptech.com.au>
2016-10-28 17:10:35 +1000

    BirdBrooder + rfidtimesheet

---
+++ BirdBrooder/BirdBrooder.ino
@@ -0,0 +1,65 @@
+#include <OneWire.h>
+
+#define RELAY1 4 // Relay on pin 4
+const float TEMPLOW = 28.00; // minimum temperature
+const float TEMPHI = 35.00; // maximum temperature
+OneWire  ds(3); // DS18B20 on pin 3
+bool lightOn; // light state
+
+void setup() {
+  pinMode(RELAY1, OUTPUT); 
+  Serial.begin(9600);
+  digitalWrite(RELAY1, LOW); // turn relay off to be safe
+  lightOn = false; // update light state
+  Serial.println("BirdBrooder v1.0 - boot up complete.");
+}
+
+
+void loop() {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius;
+  if (!ds.search(addr)) {
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);
+  delay(1000);
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);
+  for (i = 0; i < 9; i++) {
+    data[i] = ds.read();
+  }
+  int16_t raw = (data[1] << 8) | data[0];
+  byte cfg = (data[4] & 0x60);
+  if (cfg == 0x00) raw = raw & ~7; 
+  else if (cfg == 0x20) raw = raw & ~3;
+  else if (cfg == 0x40) raw = raw & ~1;
+  celsius = (float)raw / 16.0;
+
+  Serial.print(celsius);
+  Serial.println("C");
+
+  if (celsius <= TEMPLOW) {
+    if (!lightOn) {
+      lightOn = true;
+      Serial.println("Temperature dropped below threshold: Enabling light");
+      digitalWrite(RELAY1, HIGH);
+    }
+  }
+
+  if (celsius >= TEMPHI) {
+    if (lightOn) {
+      lightOn = false;
+      Serial.println("Temperature rose above threshold: Disabling light");
+      digitalWrite(RELAY1, LOW);
+    }
+  }
+}
---
+++ RFID/rfidtimesheet/rfidtimesheet.ino
@@ -0,0 +1,88 @@
+/*
+ * MFRC522 - Library to use ARDUINO RFID MODULE KIT 13.56 MHZ WITH TAGS SPI W AND R BY COOQROBOT.
+ * The library file MFRC522.h has a wealth of useful info. Please read it.
+ * The functions are documented in MFRC522.cpp.
+ *
+ * Based on code Dr.Leong   ( WWW.B2CQSHOP.COM )
+ * Created by Miguel Balboa (circuitito.com), Jan, 2012.
+ * Rewritten by Søren Thing Andersen (access.thing.dk), fall of 2013 (Translation to English, refactored, comments, anti collision, cascade levels.)
+ * Released into the public domain.
+ *
+ * Sample program showing how to read data from a PICC using a MFRC522 reader on the Arduino SPI interface.
+ *----------------------------------------------------------------------------- empty_skull 
+ * Aggiunti pin per arduino Mega
+ * add pin configuration for arduino mega
+ * http://mac86project.altervista.org/
+ ----------------------------------------------------------------------------- Nicola Coppola
+ * Pin layout should be as follows:
+ * Signal     Pin              Pin               Pin
+ *            Arduino Uno      Arduino Mega      MFRC522 board
+ * ------------------------------------------------------------
+ * Reset      9                5                 RST
+ * SPI SS     10               53                SDA
+ * SPI MOSI   11               51                MOSI
+ * SPI MISO   12               50                MISO
+ * SPI SCK    13               52                SCK
+ *
+ * The reader can be found on eBay for around 5 dollars. Search for "mf-rc522" on ebay.com. 
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include "pitches.h"
+
+#define SS_PIN 10
+#define RST_PIN 9
+#define speakerOut 3
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communications with the PC
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 card
+  pinMode(speakerOut, OUTPUT);
+  Serial.println("Scan PICC to see UID and type...");
+}
+
+void loop() {
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card. PICC_HaltA() is automatically called.
+  Serial.print("Got scan! Card ID: ");
+  
+  MFRC522::Uid cuid;
+  
+  cuid = mfrc522.uid;
+  for (byte i = 0; i < cuid.size; i++) {
+    if(cuid.uidByte[i] < 0x10)
+      Serial.print(F(" 0"));
+    else
+      Serial.print(F(" "));
+    Serial.print(cuid.uidByte[i], HEX);
+  }
+  Serial.println();
+
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(speakerOut, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+
+  delay(1000);
+}
